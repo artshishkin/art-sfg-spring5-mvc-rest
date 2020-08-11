1d4e05b23454604cc421fc3aed532435ed9bac86
admin <d.art.shishkin@gmail.com>
2020-08-11 14:37:59 +0300

    created Spring Boot project - New Spring Boot Project (374) #1

---
+++ src/main/java/com/artarkatesoft/artsfgspring5mvcrest/ArtSfgSpring5MvcRestApplication.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.artsfgspring5mvcrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSfgSpring5MvcRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtSfgSpring5MvcRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artarkatesoft/artsfgspring5mvcrest/ArtSfgSpring5MvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.artsfgspring5mvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtSfgSpring5MvcRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
