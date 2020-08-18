96ef57d238e08cea42492d0725a51d4972d5ae99
Art <d.art.shishkin@gmail.com>
2020-08-18 12:41:25 +0300

    Swagger UI (398). closes #18

--- src/main/java/com/artarkatesoft/artsfgspring5mvcrest/config/SwaggerConfig.java
+++ src/main/java/com/artarkatesoft/artsfgspring5mvcrest/config/SwaggerConfig.java
@@ -10,6 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+//public class SwaggerConfig extends WebMvcConfigurerAdapter {
 public class SwaggerConfig {
     @Bean
     public Docket api() {
@@ -20,4 +21,14 @@ public class SwaggerConfig {
                 .build()
                 .pathMapping("/");
     }
+
+    //NON Spring Boot Application
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
 }
